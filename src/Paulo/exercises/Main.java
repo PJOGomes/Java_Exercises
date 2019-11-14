3064424bf7b09606eda2dee5eede9be3f0040323
PJOGomes <36425309+PJOGomes@users.noreply.github.com>
2019-11-14 12:17:32 +0000

    Added exercise SumOdd

--- src/Paulo/exercises/Main.java
+++ src/Paulo/exercises/Main.java
@@ -3,11 +3,14 @@ package Paulo.exercises;
 public class Main {
 
     public static void main(String[] args) {
-	NumberInWord exercicio;
-        exercicio = new NumberInWord();
-        exercicio.print();
-    NumOfDays dias;
-    dias = new NumOfDays();
-        System.out.println(dias.getDaysInMonth(2, 2019));
+//	NumberInWord exercicio;
+//        exercicio = new NumberInWord();
+//        exercicio.print();
+//    NumOfDays dias;
+//    dias = new NumOfDays();
+//        System.out.println(dias.getDaysInMonth(2, 2019));
+        SumOdd soma;
+        soma = new SumOdd();
+        System.out.println(soma.sumOdd(1,100));
     }
 }
