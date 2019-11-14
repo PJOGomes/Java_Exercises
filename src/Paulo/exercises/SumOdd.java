b4e05f86faf8a5416694a16eb7036081dd125fa0
PJOGomes <36425309+PJOGomes@users.noreply.github.com>
2019-11-14 12:17:59 +0000

    Added exercise SumOdd

---
+++ src/Paulo/exercises/SumOdd.java
@@ -0,0 +1,26 @@
+package Paulo.exercises;
+
+public class SumOdd {
+    public static boolean isOdd( int number) {
+
+       if(number % 2 != 0 || number <0){
+           return true;
+       } else {
+           return false;
+       }
+    }
+
+    public static int sumOdd (int start, int end) {
+        int sum = 0;
+        if (start < 0 || end < 0 || end < start) {
+            return -1;
+        } else {
+            for (int i = start; i < end + 1; i++) {
+                if (isOdd(i)) {
+                    sum += i;
+                }
+            }
+        }
+        return sum;
+    }
+}
